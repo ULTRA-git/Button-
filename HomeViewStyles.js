0fe1050f1f09d9630b14d56330ed74df6f4677aa
ULTRA <68021867+ULTRA-git@users.noreply.github.com>
2020-08-18 08:26:51 -0500

    Add files via upload

---
+++ HomeViewStyles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const HomeViewStyles = StyleSheet.create({
+   
+    Header: {
+        marginTop:60,
+        textAlign:'center',
+        fontSize: 60,
+        color: '#000',
+        fontWeight: 'bold'
+        
+    },
+
+    Button: {
+        width:300,
+        height:300,
+        borderRadius: 150,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FF0000'
+    },
+
+    ButtonText: {
+        fontSize: 60,
+        color: '#FFF',
+        fontWeight: 'bold'
+    }
+
+    
+
+})
+
+export default HomeViewStyles;
